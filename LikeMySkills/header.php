55c30478f40b35631bfd26eca6b9ff45a6d5a5c6
JarnoPeeters <info@insideweb.nl>
2016-10-07 12:57:59 +0200

    Changing

--- LikeMySkills/header.php
+++ LikeMySkills/header.php
@@ -1,3 +1,17 @@
-<meta charset="UTF-8">
-<link rel="stylesheet" href="css/style.css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="#">Account</a></li>
+			</ul>
+		</nav>
+	</header>
+	<content>
--- LikeMySkills/index.php
+++ LikeMySkills/index.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<?php
-		include 'header.php';
-	?>
-	<title>LikeMySkills</title>
-</head>
-<body>
-	<header>
-		<nav>
-			<ul>
-				<li>Home</li>
-				<li>Mijn account</li>
-			</ul>
-		</nav>
-	</header>
-	<content>
-		<p>Test paragraph</p>
-		<p>Second paragraph</p>
-	</content>
-	<footer>
-		
-	</footer>
-</body>
-</html>
+<?php
+	include 'header.php';
+?>
+	<p>Test <a href="#">paragraph</a></p>
+	<p>Second paragraph</p>
+<?php
+	include 'footer.php';
+?>

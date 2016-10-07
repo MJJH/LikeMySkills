a6e7b879d3b096cb745046251b21f3d4306aa583
JarnoPeeters <info@insideweb.nl>
2016-10-07 10:56:05 +0200

    Structure

---
+++ LikeMySkills/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LikeMySkills</title>
+</head>
+<body>
+	<header>
+		<nav>
+			
+		</nav>
+	</header>
+	<footer>
+		
+	</footer>
+</body>
+</html>

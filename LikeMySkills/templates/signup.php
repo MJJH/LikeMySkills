eb01268ccd7adde7ea4cc456bf190cc34eac6b9b
fontys-nm <n.massaad@student.fontys.nl>
2016-11-03 14:55:13 +0100

    Add files via upload

---
+++ LikeMySkills/templates/signup.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<html>
+<head>
+</head>
+<body>
+<form>
+	
+	<label for="username">Username:</label>
+	<input type="text" name="username" /><br>
+	
+	<label for="password">Password:</label>
+	<input type="password" name="password" /><br>
+	
+	<label for="email">E-mail:</label>
+	<input type="text" name="email" /><br>
+	<input type="submit" value="Registreer">
+</form>
+</body>
+</html>

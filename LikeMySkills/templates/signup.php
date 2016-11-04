2a7f896e6fe624a36455bddaf49874df0de3dc42
fontys-nm <n.massaad@student.fontys.nl>
2016-11-04 12:24:44 +0100

    Added signup handling

--- LikeMySkills/templates/signup.php
+++ LikeMySkills/templates/signup.php
@@ -1,10 +1,33 @@
 <?php
+$emptyform = false;
+if (isset($_POST)){
+	if (isset($_POST["username"])){
+		if empty($_POST["username"]){
+			$emptyform = true;
+			$_error[] = "%EmptyUsername%";
+		}
+	}
+	if (isset($_POST["password"])){
+		if empty($_POST["password"]){
+			$emptyform = true;
+			$_error[] = "%EmptyPassword%";
+		}
+	}
+	if (isset($_POST["email"])){
+		if empty($_POST["email"]){
+			$emptyform = true;
+			$_error[] = "%EmptyEmail%";
+		}
+	}
+	if (!$emptyform){
+		User::register($_POST["username"],$_POST["password"],$_POST["email"]);
+	}
+	else{
+		print"Een van de velden is leeg";
+	}
+}
 ?>
-<html>
-<head>
-</head>
-<body>
-<form>
+<form method="post" action="index.php?page=signup">
 	
 	<label for="username">Username:</label>
 	<input type="text" name="username" /><br>
@@ -16,5 +39,3 @@
 	<input type="text" name="email" /><br>
 	<input type="submit" value="Registreer">
 </form>
-</body>
-</html>

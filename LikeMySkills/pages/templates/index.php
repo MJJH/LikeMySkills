d8399530b242943dd54feb8cbf2f36664a8b6c10
PimDieks <pimdieks@gmail.com>
2016-11-10 15:06:20 +0100

    loader
    loader created

--- LikeMySkills/pages/templates/index.php
+++ LikeMySkills/pages/templates/index.php
@@ -1,4 +1,20 @@
+<link rel="stylesheet" href="css/style.css">
+<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
 <div class="container">
+
+	<div id="loader-wrapper">
+		<div id="loader"></div>
+
+		<div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+
+ 	</div>
+
 	<b>%back%</b>
 	
 </div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="js/loader.js"></script>

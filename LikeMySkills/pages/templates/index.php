6746ba84c138e383f91cd1c89f9257ce17d3c094
Pim Dieks <pimdieks@Pims-MacBook-Pro.local>
2016-11-24 13:17:28 +0100

    loader

--- LikeMySkills/pages/templates/index.php
+++ LikeMySkills/pages/templates/index.php
@@ -1,7 +1,7 @@
 <div class="container">
 
 	<div id="loader-wrapper">
-		<div id="loader"></div>
+		<div class="loader"></div>
 
         <div class="loader-text"><h6>SPORT REPORT</h6></div>
 		<div class="loader-section section-left"></div>
